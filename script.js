ef80119d71b05e5c363a905313b69133b14538dd
f1shu <filipszulzycki@interia.pl>
2023-03-02 20:40:48 +0100

    02 MAR 2023

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var currentID = 1, currentWord = '', length = 0;
 var wordToGuess;
 const keys = document.querySelectorAll('.key');
-const resetButton = document.getElementById('reset');
-const giveUpButton = document.getElementById('give_up');
+const utilityKeys = document.querySelectorAll('.utility');
 
+// reading words database and picking a random word
 fetch('data.txt')
     .then(response => response.text())
     .then(text => {
@@ -12,31 +12,10 @@ fetch('data.txt')
         console.log(`Random word: ${wordToGuess}`);
     })
 
+// virtual keyboard input
 keys.forEach(key => {
     key.addEventListener('click', function () {
-        if (key.id === 'delete' && currentID > 1) {
-            document.getElementById(currentID - 1).textContent = '';
-            currentWord = currentWord.slice(0, -1);
-            length--;
-            if (currentID > 0) currentID--;
-        } else if (key.id === 'enter') {
-            if (((currentID - 1) % 5 !== 0)) alert('Za mało znaków!');
-            else {
-                var temporaryWord = currentWord.toLowerCase();
-                fetch('data.txt')
-                    .then(response => response.text())
-                    .then(text => {
-                        if (text.includes(temporaryWord)) {
-                            if (temporaryWord === wordToGuess) {
-                                if(confirm('Gratulacje! Chcesz rozpocząć nową grę?')) location.reload();
-                            }
-                        }
-                        else alert('Podane słowo nie istnieje!');
-                    });
-                currentWord = '';
-                length = 0;
-            }
-        } else if (key.id !== 'delete' && key.id !== 'enter' && length < 5) {
+        if (length < 5) {
             document.getElementById(currentID).textContent = key.textContent;
             currentWord += key.textContent;
             length++;
@@ -45,13 +24,38 @@ keys.forEach(key => {
     })
 });
 
-resetButton.addEventListener('click', function () {
-    if (confirm('Czy na pewno?')) location.reload();
-});
-
-giveUpButton.addEventListener('click', function () {
-    if (confirm('Czy na pewno?')) {
-        alert('Porażka! Ukryte słowo: ' + wordToGuess)
-        location.reload();
-    }
+// utility keys input
+utilityKeys.forEach(key => {
+    key.addEventListener('click', function () {
+        if (key.id === 'delete' && currentID > 1) {
+            document.getElementById(currentID - 1).textContent = '';
+            currentWord = currentWord.slice(0, -1);
+            length--;
+            if (currentID > 0) currentID--;
+        } else if (key.id === 'enter') {
+            if (((currentID - 1) % 5 !== 0) || currentID === 1) alert('Za mało znaków!');
+            else {
+                var temporaryWord = currentWord.toLowerCase();
+                fetch('data.txt')
+                    .then(response => response.text())
+                    .then(text => {
+                        if (text.includes(temporaryWord)) {
+                            if (temporaryWord === wordToGuess) {
+                                if (confirm('Gratulacje! Chcesz rozpocząć nową grę?')) location.reload();
+                            }
+                        }
+                        else alert('Podane słowo nie istnieje!');
+                    });
+                currentWord = '';
+                length = 0;
+            }
+        } else if (key.id === 'reset') {
+            if (confirm('Czy na pewno?')) location.reload();
+        } else if (key.id === 'give_up') {
+            if (confirm('Czy na pewno?')) {
+                alert('Porażka! Ukryte słowo: ' + wordToGuess)
+                location.reload();
+            }
+        }
+    })
 });
